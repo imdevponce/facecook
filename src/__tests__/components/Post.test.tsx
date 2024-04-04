259d1e95bdb4ff93943724ed3c014349faf98d94
imdevponce <imdevponce@gmail.com>
2024-04-04 16:04:16 -0600

    post test added

--- src/__tests__/components/Post.test.tsx
+++ src/__tests__/components/Post.test.tsx
@@ -2,6 +2,14 @@ import { render } from '@testing-library/react'
 import Post from '@/components/Post'
 import { post1 } from '@/mocks/postsData'
 
-it('Must render Post component', () => {
+it('Must render Post component with data', () => {
     const component = render(<Post post={post1} />)
+    const name = component.getByText(post1.username)
+    const description = component.getByText(post1.description)
+    const location = component.getByText(post1.location)
+    const time = component.getByText(post1.time)
+    expect(name).toBeDefined()
+    expect(description).toBeDefined()
+    expect(location).toBeDefined()
+    expect(time).toBeDefined()
 })
