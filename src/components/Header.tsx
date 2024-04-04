6271d0bbcebbd5e9dfbd12846f67b1158ec23ff5
imdevponce <imdevponce@gmail.com>
2024-04-04 13:39:14 -0600

    feature: added mocks and profile page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 'use client'
 import { useState } from 'react'
+import { useAtom } from 'jotai'
+import { userAtom } from '@/atoms/userAtom'
 import styles from "./header.module.css";
 import Image from "next/image";
 import MenuIcon from './icons/MenuIcon'
 import UserIcon from './icons/UserIcon'
 import Link from "next/link";
 export default function Header() {
+  const [userData] = useAtom(userAtom)
   const [displayUserOptions, setDisplayUserOptions] = useState(false)
   const onHandleClick = () => {
     setDisplayUserOptions(!displayUserOptions)
@@ -17,10 +20,11 @@ export default function Header() {
           <Image className={styles.facecookLogo} src="/facecook-logo.png" alt="FaceCook logo" height={52} width={130}/>
         </Link>
         <div className={styles.userOptionsContainer}>
+          <p className={styles.username}>{userData.username}</p>
           <UserIcon height={30} width={30} onClick={onHandleClick}/>
           {displayUserOptions ? (
             <ul className={styles.userOptions}>
-              <Link href='/profile' className={styles.link} >
+              <Link href={`/profile/${userData.id}`} className={styles.link} >
                 <li >My profile</li>
               </Link>
             </ul>) 
